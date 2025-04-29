1daa1c3c122d6ca6c19a06df481c81857a426aa3
danaralifian <danaralifian@gmail.com>
2025-04-29 16:33:47 +0700

    feat: crud user and authorization

---
+++ src/app.module.ts
@@ -0,0 +1,43 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModule } from './users/user.module';
+import { User } from './users/entities/user.entity';
+import { AuthModule } from './auth/auth.module';
+import { APP_GUARD } from '@nestjs/core';
+import { JwtGuard } from './auth/jwt.guard';
+import { JwtStrategy } from './auth/jwt.strategy';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, // Makes env variables accessible globally
+      envFilePath: '.env', // Specify the env file (default: `.env`)
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.SUPABASE_HOST,
+      port: Number(process.env.SUPABASE_PORT),
+      username: process.env.SUPABASE_USER,
+      password: process.env.SUPABASE_PASSWORD,
+      database: process.env.SUPABASE_DB,
+      autoLoadEntities: process.env.SUPABASE_ENVIRONTMENT === 'development', // Set to false in production
+      synchronize: true,
+      entities: [User],
+    }),
+    UserModule,
+    AuthModule,
+  ],
+  controllers: [AppController],
+  providers: [
+    AppService,
+    {
+      provide: APP_GUARD,
+      useClass: JwtGuard,
+    },
+    JwtStrategy,
+  ],
+})
+export class AppModule {}
---
+++ src/users/user.controller.ts
@@ -0,0 +1,42 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  Query,
+} from '@nestjs/common';
+import { UserService } from './user.service';
+import { UserDto } from './dto/user.dto';
+
+@Controller('users')
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Post()
+  create(@Body() createUserDto: UserDto) {
+    return this.userService.create(createUserDto);
+  }
+
+  @Get()
+  findAll(@Query('page') page: number = 1, @Query('limit') limit: number = 10) {
+    return this.userService.findAll(page, limit);
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.userService.findOne(+id);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateUserDto: UserDto) {
+    return this.userService.update(+id, updateUserDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.userService.remove(+id);
+  }
+}
